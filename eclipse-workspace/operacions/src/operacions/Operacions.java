3e5286f6070d9a180a9b6800607df814fb3c49cc
Alberto Bergantiños <alberto.bm3197@gmail.com>
2020-04-07 13:19:40 +0200

    Se añaden las dos clases muevas

--- eclipse-workspace/operacions/src/operacions/Operacions.java
+++ eclipse-workspace/operacions/src/operacions/Operacions.java
@@ -2,7 +2,7 @@ package operacions;
 
 import java.util.Scanner;
 
-public class Operacions {
+public class Operacions2 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
