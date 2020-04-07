c06ac53b34a03f7efeea90d047b27920b2ab2306
Alberto Bergantiños <alberto.bm3197@gmail.com>
2020-04-07 13:07:23 +0200

    Añado a la rama master, las clases operacions y factorian en donde en la

---
+++ eclipse-workspace/operacions/src/operacions/Operacions.java
@@ -0,0 +1,27 @@
+package operacions;
+
+import java.util.Scanner;
+
+public class Operacions {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		factorialIterativo(sc);
+
+	}
+
+	private static void factorialIterativo(Scanner sc) {
+		System.out.println("Este programa sirve para calcular el factorial de un entero positivo de manera iterativa");
+		System.out.println("De que numero quieres calcular el factorial: ");
+		int numeroACaluclar = sc.nextInt();
+
+		Factorial fact = new Factorial();
+		try {
+			double valor = fact.getIterativa(numeroACaluclar);
+			System.out.println(valor);
+		} catch (IllegalArgumentException e) {
+			System.out.println("No se pueden utilizar nombres negativos.");
+		}
+	}
+
+}
